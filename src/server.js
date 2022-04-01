d119215d3888fdc39fb0221b5aee5e9135886405
thlanza <thlanza@hotmail.com>
2022-04-01 16:30:15 -0300

    axios com config padrão

--- src/server.js
+++ src/server.js
@@ -33,5 +33,29 @@ axios({
     data: {
         grant_type: 'client_credentials'
     }
-}).then((response) => console.log(response.data));
+}).then((response) => {
+    const accessToken = response.data?.access_token;
+
+    const reqGN = axios.create({
+        baseURL: process.env.GN_ENDPOINT,
+        httpsAgent: agent,
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const dataCob = {
+        calendario: {
+            "expiracao": 3600
+        },
+        valor: {
+            "original": "100.00"
+        },
+        chave: "099.407.576-62",
+        solicitacaoPagador: "Cobrança"
+    };
+
+    reqGN.post('/v2/cob', dataCob).then((response => console.log(response)));
+});
 
