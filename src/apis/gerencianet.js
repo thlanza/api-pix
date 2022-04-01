5ddd88d185e8516146cbb1b9c9226ce7904f8e4f
thlanza <thlanza@hotmail.com>
2022-04-01 17:46:48 -0300

    ainda refatorando

--- src/apis/gerencianet.js
+++ src/apis/gerencianet.js
@@ -12,11 +12,14 @@ const agent = new https.Agent({
     passphrase: ''
 });
 
-const credentials = Buffer.from(
-`${process.env.GN_CLIENT_ID}:${process.env.GN_CLIENT_SECRET}`
-).toString('base64');
 
-const authenticate = () => {
+
+const authenticate = ({ clientID, clientSecret }) => {
+
+    const credentials = Buffer.from(
+        `${clientID}:${clientSecret}`
+        ).toString('base64');
+
     return axios({
         method: 'POST',
         url: `${process.env.GN_ENDPOINT}/oauth/token`,
@@ -32,8 +35,8 @@ const authenticate = () => {
     });
 };
 
-const GNRequest = async () => {
-    const authResponse = await authenticate();
+const GNRequest = async (credentials) => {
+    const authResponse = await authenticate(credentials);
     const accessToken = authResponse.data?.access_token;
     
     return axios.create({
